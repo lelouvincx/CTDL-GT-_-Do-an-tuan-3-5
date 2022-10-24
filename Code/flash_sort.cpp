94a997c019c556660397fc666ed5001b8a3c38fb
lelouvincx <dinhminhchinh3357@gmail.com>
2022-10-24 20:46:55 +0700

    update flash sort

--- Code/flash_sort.cpp
+++ Code/flash_sort.cpp
@@ -6,31 +6,59 @@ const int MAX = 1e6;
 const int MAX2 = 450010;
 int last_index[MAX2], number_of_elements_in_class[MAX2];
 
-int binary_search(int a[], int val, int left, int right) {
-  if (right <= left) {
-    if (val > a[left])
-      return left + 1;
-    else
-      return left;
+void insertion_sort(int a[], int n) {
+  int k = 1;
+  while (k < n) {
+    int x = a[k];
+    int pos = k - 1;
+    while (a[pos] > x && pos >= 0) {
+      a[pos + 1] = a[pos];
+      pos--;
+    }
+    a[pos + 1] = x;
+    k++;
   }
-  int mid = left + (right - left) / 2;
-  if (val == a[mid])
-    return mid + 1;
-  if (val < a[mid])
-    return binary_search(a, val, left, mid - 1);
-  return binary_search(a, val, mid + 1, right);
 }
 
-void binary_insertion_sort(int a[], int left, int right) {
-  for (int i = left + 1; i <= right; ++i) {
-    int j = i - 1;
-    int selected = a[i];
-    int loc = binary_search(a, selected, 0, j);
-    while (j >= loc) {
-      a[j + 1] = a[j];
-      j--;
+void merge(int arr[], int left, int mid, int right) {
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
+  int LEFT[n1], RIGHT[n2];
+  for (int i = 0; i < n1; i++)
+    LEFT[i] = arr[left + i];
+  for (int j = 0; j < n2; j++)
+    RIGHT[j] = arr[mid + 1 + j];
+  int i = 0;
+  int j = 0;
+  int k = left;
+  while (i < n1 && j < n2) {
+    if (LEFT[i] <= RIGHT[j]) {
+      arr[k] = LEFT[i];
+      i++;
+    } else {
+      arr[k] = RIGHT[j];
+      j++;
     }
-    a[j + 1] = selected;
+    k++;
+  }
+  while (i < n1) {
+    arr[k] = LEFT[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    arr[k] = RIGHT[j];
+    j++;
+    k++;
+  }
+}
+
+void merge_sort(int arr[], int left, int right) {
+  if (left < right) {
+    int mid = left + (right - left) / 2;
+    merge_sort(arr, left, mid);
+    merge_sort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
   }
 }
 
@@ -39,6 +67,7 @@ int calculate_class(int x, int m, int min_val, int max_val) {
 }
 
 void flash_sort(int a[], int n) {
+  // Step 1: Classification
   int min_val, max_val;
   min_val = max_val = a[0];
   for (int i = 1; i < n; ++i) {
@@ -48,7 +77,7 @@ void flash_sort(int a[], int n) {
       max_val = a[i];
   }
 
-  int m = 0.45 * n;
+  int m = 0.43 * n;
   last_index[0] = -1;
   for (int i = 0; i < n; ++i) {
     int tmp = calculate_class(a[i], m, min_val, max_val);
@@ -58,7 +87,7 @@ void flash_sort(int a[], int n) {
   for (int i = 1; i < m; ++i)
     last_index[i] += last_index[i - 1];
 
-  int number_of_moves = n;
+  int number_of_moves = n - 1;
   for (int moves = 0; moves < number_of_moves; ++moves) {
     int tmp = calculate_class(a[0], m, min_val, max_val);
     if (last_index[tmp] >= 0)
@@ -68,12 +97,25 @@ void flash_sort(int a[], int n) {
     last_index[tmp]--;
   }
 
-  int left = 0, right = -1, i = 0;
-  do {
-    left = right + 1;
-    right = left + number_of_elements_in_class[i++] - 1;
-    binary_insertion_sort(a, left, right);
-  } while (i < m);
+  // insertion sort
+  // int k = 1;
+  // while (k < n) {
+  //   int x = a[k];
+  //   int pos = k - 1;
+  //   while (a[pos] > x && pos >= 0) {
+  //     a[pos + 1] = a[pos];
+  //     pos--;
+  //   }
+  //   a[pos + 1] = x;
+  //   k++;
+  // }
+  // insertion_sort(a, n);
+  // int left = 0, right = -1, i = 0;
+  // do {
+  //   left = right + 1;
+  //   right = left + number_of_elements_in_class[i++] - 1;
+  //   merge_sort(a, left, right);
+  // } while (i < m);
 }
 
 int main() {
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CTDL-GT-_-Do-an-tuan-3-5
-Do an mon thuc hanh CTDL&GT tuan 3-5
+Source code của đồ án Thực hành cấu trúc dữ liệu và giải thuật được lưu tại đây.
+
+## Thành phần
+- ./Code: Thư mục chứa source code của các thuật toán search và sort
+- ./Tests: Thư mục chứa test theo yêu cầu và code CPP để sinh test
+- ./Results: Chứa các file *.txt lưu kết quả chạy
+- ./Images: Chứa các hình ảnh được dùng trong báo cáo
+- Runtime.csv: Dữ liệu kết quả chạy các thuật toán sort theo thời gian tương ứng với từng test
+- script.ipynb: File jupyter notebook lưu cách tư động biên dịch, chạy, xuất kết quả vào csv và trực quan hóa lên biểu đồ bằng matplotlib
+
+## Sở hữu
+Các nội dung trên là thuộc sở hữu của Đinh Minh Chính (MSSV 21280007) và Lê Trần Bảo Ngọc (MSSV 21280036). Được phép tham khảo và copy với mục đích phi lợi nhuận. Xin cảm ơn.
